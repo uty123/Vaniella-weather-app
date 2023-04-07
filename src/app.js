80e7c9473ae9bbfd6c2c177d3fa1c45bee08ef05
Utybby <35872931+uty123@users.noreply.github.com>
2023-04-07 13:26:05 +0100

    Showed current date display

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
+function formateDate(timestamp) {
+  //calculate the date and return something like "Friday 5:00"
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wedesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeatherForecast(response) {
-  console.log(response.data.daily[0].wind.speed);
+  //console.log(response.data.daily[0].time * 1000);
   let temperatureElementMonday = document.querySelector("#temp-monday");
   let cityElement = document.querySelector("#city");
   let weatherDescription = document.querySelector("#weather-description");
   let humidityElement = document.querySelector("#humidity-element");
   let windElement = document.querySelector("#wind-element");
+  let dateElement = document.querySelector("#date");
+
+  dateElement.innerHTML = formateDate(response.data.daily[0].time * 1000);
   windElement.innerHTML = Math.round(response.data.daily[0].wind.speed);
   humidityElement.innerHTML = Math.round(
     response.data.daily[0].temperature.humidity
@@ -19,6 +46,6 @@ function displayWeatherForecast(response) {
 apiKey = "d0aaf35fd7f0bc76394a85b20o4aeft8";
 query = "Nigeria";
 apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${query}&key=${apiKey}&units=metric`;
-console.log(apiUrl);
+//console.log(apiUrl);
 
 axios.get(apiUrl).then(displayWeatherForecast);
