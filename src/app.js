b552a2d396fc275e9a9b5c4deda3e6b132eb2fd0
Utybby <35872931+uty123@users.noreply.github.com>
2023-04-07 16:22:38 +0100

    Using relevant icons from the API

--- src/app.js
+++ src/app.js
@@ -30,7 +30,16 @@ function displayWeatherForecast(response) {
   let humidityElement = document.querySelector("#humidity-element");
   let windElement = document.querySelector("#wind-element");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
+  //setting icon elements for other days of the week
+  let iconElementTue = document.querySelector("#icon-tue");
+  let iconElementWed = document.querySelector("#icon-wed");
+  let iconElementThu = document.querySelector("#icon-thu");
+  let iconElementFri = document.querySelector("#icon-fri");
+  let iconElementSat = document.querySelector("#icon-sat");
+
+  //setting temperature for other days of the week
   let temperatureElementTue = document.querySelector("#temp-tue");
   let temperatureElementWed = document.querySelector("#temp-wed");
   let temperatureElementThu = document.querySelector("#temp-thu");
@@ -53,6 +62,35 @@ function displayWeatherForecast(response) {
     response.data.daily[6].temperature.day
   );
 
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[1].condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.daily[1].condition.description);
+
+  //setting icon element attributes for other days of the week
+
+  iconElementTue.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[2].condition.icon}.png`
+  );
+  iconElementWed.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[3].condition.icon}.png`
+  );
+  iconElementThu.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[4].condition.icon}.png`
+  );
+  iconElementFri.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[5].condition.icon}.png`
+  );
+  iconElementSat.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.daily[6].condition.icon}.png`
+  );
+
   dateElement.innerHTML = formateDate(response.data.daily[1].time * 1000);
   windElement.innerHTML = Math.round(response.data.daily[1].wind.speed);
   humidityElement.innerHTML = Math.round(
@@ -66,7 +104,7 @@ function displayWeatherForecast(response) {
 }
 
 apiKey = "d0aaf35fd7f0bc76394a85b20o4aeft8";
-query = "Nigeria";
+query = "Tokyo";
 apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${query}&key=${apiKey}&units=metric`;
 //console.log(apiUrl);
 
